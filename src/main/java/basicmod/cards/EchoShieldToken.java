db32d31caa80999e95ea3dfdc28fc18b735c3480
DESKTOP-ITVP7RT\rowan <rowan.clover@gmail.com>
2023-09-04 20:18:50 -0700

    added a buncha card files and updated card strings CombinationBlow EchoShield echoshield VoidWallGrenade

---
+++ src/main/java/basicmod/cards/EchoShieldToken.java
@@ -0,0 +1,47 @@
+package basicmod.cards;
+
+
+import basicmod.character.MyCharacter;
+import basicmod.util.CardStats;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class EchoShieldToken extends BaseCard {
+
+    public static final String ID = makeID("EchoShieldToken");
+    private static final CardStats info = new CardStats(
+            MyCharacter.Enums.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            1
+    );
+
+
+    private static final int BLOCK = 8;
+    private static final int UPG_BLOCK = 3;
+
+    public EchoShieldToken() {
+        super(ID, info);
+        setBlock(BLOCK, UPG_BLOCK);
+        setEthereal(true);
+        setExhaust(true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, p, block));
+        addToBot(new MakeTempCardInHandAction(new EchoShieldToken(), 1));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new EchoShieldToken();
+    }
+}
