716cf39183be988d8a993bd67b9a1309506f6989
GLOWY\Me <moutmesguine@cpp.edu>
2023-09-17 00:25:32 -0700

    Made keyword for Charge of Light Power Added deadshot card and power images Fixed spelling of TravelersIncandescence

---
+++ src/main/java/basicmod/powers/DeadshotPower.java
@@ -0,0 +1,36 @@
+package basicmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+
+import static basicmod.TheLightbearer.makeID;
+
+
+public class DeadshotPower extends BasePower implements CloneablePowerInterface {
+    public static final String POWER_ID = makeID("DeadshotPower");
+    private static final PowerType TYPE = PowerType.BUFF;
+    private static final boolean TURN_BASED = false;
+
+
+    public DeadshotPower(AbstractCreature owner, int amount) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, amount);
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new DeadshotPower(owner, amount);
+    }
+
+    @Override
+    public void atStartOfTurnPostDraw() {
+        super.atStartOfTurnPostDraw();
+        addToBot(new ApplyPowerAction(this.owner, this.owner, new VigorPower(this.owner, this.amount)));
+    }
+}
--- src/main/java/basicmod/relics/TheTraveler.java
+++ src/main/java/basicmod/relics/TheTraveler.java
@@ -1,17 +1,10 @@
 package basicmod.relics;
 
-import basemod.patches.com.megacrit.cardcrawl.relics.AbstractRelic.RelicOutlineColor;
-import basicmod.TheLightbearer;
 import basicmod.character.MyCharacter;
 import basicmod.powers.ChargeOfLightPower;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
@@ -33,8 +26,8 @@ public class TheTraveler extends BaseRelic{
         return DESCRIPTIONS[0];
     }
 
-    public void atTurnStart(){
-        addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player,
+    public void atTurnStartPostDraw(){
+        addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player,
                 new ChargeOfLightPower(AbstractDungeon.player, 1)));
     }
 /*
