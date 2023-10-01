fa2d65c757a1bc94e3a8c48729a33a41ee0c484d
GLOWY\Me <moutmesguine@cpp.edu>
2023-10-01 00:00:33 -0700

    Arc Supers

--- src/main/java/basicmod/cards/ArcStaff.java
+++ src/main/java/basicmod/cards/ArcStaff.java
@@ -1,48 +1,47 @@
 package basicmod.cards;
 
-
 import basicmod.character.MyCharacter;
-import basicmod.powers.SpectralBladesPower;
+import basicmod.powers.ArcStaffPower;
 import basicmod.util.CardStats;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.DexterityPower;
 
+import static basicmod.util.CustomTags.ARC;
 import static basicmod.util.CustomTags.SUPERSPELL;
-import static basicmod.util.CustomTags.VOID;
 
-public class SpectralBlades extends BaseCard {
+public class ArcStaff extends BaseCard {
+    public static final String ID = makeID("ArcStaff");
 
-    public static final String ID = makeID("SpectralBlades");
+
     private static final CardStats info = new CardStats(
             MyCharacter.Enums.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
-            CardType.POWER, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            4
+            4 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
 
     private static final int MAGIC_NUMBER = 2;
     private static final int UPG_MAGIC_NUMBER = 1;
 
-    public SpectralBlades() {
-        super(ID, info);
+    public ArcStaff() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+        setMagic(MAGIC_NUMBER, UPG_MAGIC_NUMBER);
         tags.add(SUPERSPELL);
-        tags.add(VOID);
-        setMagic(MAGIC_NUMBER,UPG_MAGIC_NUMBER);
+        tags.add(ARC);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(p,p,new SpectralBladesPower(p,this.magicNumber)));
-        addToBot(new ApplyPowerAction(p,p,new StrengthPower(p,3),3));
-
+        addToBot(new ApplyPowerAction(p,p,new ArcStaffPower(p,this.magicNumber),this.magicNumber));
+        addToBot(new ApplyPowerAction(p,p,new DexterityPower(p,3),3));
     }
 
     @Override
-    public AbstractCard makeCopy() {
-        return new SpectralBlades();
+    public AbstractCard makeCopy() { //Optional
+        return new ArcStaff();
     }
 }
--- src/main/java/basicmod/cards/GatheringStorm.java
+++ src/main/java/basicmod/cards/GatheringStorm.java
@@ -1,6 +1,7 @@
 package basicmod.cards;
 
 import basicmod.character.MyCharacter;
+import basicmod.powers.GatheringStormPower;
 import basicmod.util.CardStats;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
@@ -9,41 +10,44 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
-public class DisorientingBlow extends BaseCard {
-    public static final String ID = makeID("DisorientingBlow");
+import static basicmod.util.CustomTags.ARC;
+import static basicmod.util.CustomTags.SUPERSPELL;
+
+public class GatheringStorm extends BaseCard {
+    public static final String ID = makeID("GatheringStorm");
 
 
     private static final CardStats info = new CardStats(
             MyCharacter.Enums.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
             CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
-            CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            4 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
 
-    private static final int DAMAGE = 8;
-    private static final int UPG_DAMAGE = 3;
+    private static final int DAMAGE = 20;
+    private static final int UPG_DAMAGE = 8;
 
-    private static final int MAGIC_NUMBER = 2;
+    private static final int MAGIC_NUMBER = 10;
+    private static final int UPG_MAGIC_NUMBER = 4;
 
-
-    public DisorientingBlow() {
+    public GatheringStorm() {
         super(ID, info); //Pass the required information to the BaseCard constructor.
-
+        tags.add(SUPERSPELL);
+        tags.add(ARC);
         setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it changes when upgraded.
-        setMagic(MAGIC_NUMBER);
+        setMagic(MAGIC_NUMBER, UPG_MAGIC_NUMBER);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
-        addToBot(new ApplyPowerAction(m, p, new WeakPower(m, this.magicNumber, false), this.magicNumber));
+        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.LIGHTNING));
+        addToBot(new ApplyPowerAction(m, p, new GatheringStormPower(m, this.magicNumber), this.magicNumber));
     }
 
     @Override
     public AbstractCard makeCopy() { //Optional
-        return new DisorientingBlow();
+        return new GatheringStorm();
     }
 }
