ca00775fbef2a9da7af638d9826df1ffb70fccf5
GLOWY\Me <moutmesguine@cpp.edu>
2024-09-18 23:08:15 -0700

    Changed FlareBlast to give temp HP

--- src/main/java/TheLightbearer/cards/Solar/FlareBlast.java
+++ src/main/java/TheLightbearer/cards/Solar/FlareBlast.java
@@ -4,11 +4,13 @@ package TheLightbearer.cards.Solar;
 import TheLightbearer.cards.BaseCard;
 import TheLightbearer.character.LightbearerCharacter;
 import TheLightbearer.util.CardStats;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static TheLightbearer.util.CustomTags.SOLAR;
@@ -24,19 +26,35 @@ public class FlareBlast extends BaseCard {
             CardTarget.ALL_ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
             1
     );
-    private static final int DAMAGE = 7;
-    private static final int UPG_DAMAGE = 3;
+    private static final int DAMAGE = 4;
+    private static final int UPG_DAMAGE = 2;
+
+    private static final int MAGIC_NUMBER = 1;
+    private static final int UPG_MAGIC_NUMBER = 0;
 
 
     public FlareBlast() {
         super(ID, info, "solar");
         setDamage(DAMAGE, UPG_DAMAGE);
+        setMagic(MAGIC_NUMBER, UPG_MAGIC_NUMBER);
         tags.add(SOLAR);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAllEnemiesAction(p,this.damage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.LIGHTNING)) ;
+        addToBot(new DamageAllEnemiesAction(p,this.damage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL)) ;
+        int aliveMonsters = 0;
+        for(AbstractMonster monster : AbstractDungeon.getCurrRoom().monsters.monsters){
+            if(!monster.isDead)
+                aliveMonsters++;
+        }
+
+
+        if(!AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()){
+            addToBot(new AddTemporaryHPAction(AbstractDungeon.player,AbstractDungeon.player, this.magicNumber * aliveMonsters));
+        }
+
+        /*addToBot(new DamageAllEnemiesAction(p,this.damage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.LIGHTNING)) ;
         for (int i = 0; i < player.hand.size(); i++) {
             AbstractCard c = player.hand.group.get(i);
             if(c.cardID == "Burn" ){
@@ -45,6 +63,7 @@ public class FlareBlast extends BaseCard {
             }
 
         }
+        */
     }
 
     @Override
