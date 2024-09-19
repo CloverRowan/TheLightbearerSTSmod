048c750be58aa1e7291ccff915ea5a9b323a2ef3
GLOWY\Me <moutmesguine@cpp.edu>
2024-09-18 23:26:05 -0700

    Changed InvigoratingRoar to exhaust and give Temp HP

--- src/main/java/TheLightbearer/cards/Solar/InvigoratingRoar.java
+++ src/main/java/TheLightbearer/cards/Solar/InvigoratingRoar.java
@@ -4,9 +4,12 @@ package TheLightbearer.cards.Solar;
 import TheLightbearer.cards.BaseCard;
 import TheLightbearer.character.LightbearerCharacter;
 import TheLightbearer.util.CardStats;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.status.Burn;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -23,22 +26,28 @@ public class InvigoratingRoar extends BaseCard {
             CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            0
+            1
     );
-    private static final int MAGIC_NUMBER = 3;
-    private static final int UPG_MAGIC_NUMBER = 3;
+    //private static final int DAMAGE = 2;
+    private static final int MAGIC_NUMBER = 6;
+    private static final int UPG_MAGIC_NUMBER = 2;
 
     public InvigoratingRoar() {
         super(ID, info, "solar");
+       // setDamage(DAMAGE);
         setMagic(MAGIC_NUMBER,UPG_MAGIC_NUMBER);
         tags.add(SOLAR);
+        setExhaust(true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        //addToBot(new DamageAction(p,new DamageInfo(p,this.damage, DamageInfo.DamageType.NORMAL)));
+
+        addToBot(new AddTemporaryHPAction(p, p, this.magicNumber));
         addToBot(new ApplyPowerAction(p,p,new StrengthPower(p, this.magicNumber), this.magicNumber));
         addToBot(new ApplyPowerAction(p,p,new LoseStrengthPower(p, this.magicNumber), this.magicNumber));
-        addToBot(new MakeTempCardInDiscardAction(new Burn(), 1));
+
     }
 
     @Override
