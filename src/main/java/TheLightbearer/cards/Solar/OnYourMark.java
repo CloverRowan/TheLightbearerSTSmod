84922a242af4e6b0930e3edd9d27fcc5dd846633
GLOWY\Me <moutmesguine@cpp.edu>
2025-02-04 14:33:08 -0800

    Updated OnYourMark to count solar cards and give temp strength and vigor

--- src/main/java/TheLightbearer/cards/Solar/OnYourMark.java
+++ src/main/java/TheLightbearer/cards/Solar/OnYourMark.java
@@ -3,11 +3,14 @@ package TheLightbearer.cards.Solar;
 import TheLightbearer.cards.BaseCard;
 import TheLightbearer.character.LightbearerCharacter;
 import TheLightbearer.util.CardStats;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static TheLightbearer.util.CustomTags.SOLAR;
@@ -21,11 +24,11 @@ public class OnYourMark extends BaseCard {
             CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
 
-    private static final int MAGIC_NUMBER = 3;
-    private static final int UPG_MAGIC_NUMBER = 1;
+    private static final int MAGIC_NUMBER = 1;
+    private static final int UPG_MAGIC_NUMBER = 0;
 
     public OnYourMark() {
         super(ID, info, "solar"); //Pass the required information to the BaseCard constructor.
@@ -33,54 +36,67 @@ public class OnYourMark extends BaseCard {
         tags.add(SOLAR);
         this.rawDescription = this.cardStrings.DESCRIPTION + this.cardStrings.EXTENDED_DESCRIPTION[0] + 0 + this.cardStrings.EXTENDED_DESCRIPTION[1];
         this.initializeDescription();
+        setCustomVar("ONYOURMARKCOUNT",0);
+        setExhaust(true,false);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        int burnCount = countBurns();
-        if(burnCount > 0){
-            addToBot(new ApplyPowerAction(p,p,new VigorPower(p,this.magicNumber*burnCount), this.magicNumber*burnCount));
+
+        int solarCount = countSolar();
+        if(solarCount > 0){
+            addToBot(new ApplyPowerAction(p, p, new StrengthPower(p,this.magicNumber*solarCount), this.magicNumber*solarCount));
+            addToBot(new ApplyPowerAction(p, p, new LoseStrengthPower(p,this.magicNumber*solarCount), this.magicNumber*solarCount));
+            addToBot(new ApplyPowerAction(p, p, new VigorPower(p, this.magicNumber*solarCount), this.magicNumber*solarCount));
         }
+
     }
 
-    private int countBurns(){
-        int burnCount = 0;
+    private int countSolar(){
+        int count = 0;
         for(AbstractCard c : AbstractDungeon.player.drawPile.group){
-            if(c.cardID.equals("Burn"))
-                burnCount++;
+            if(c.tags.contains(SOLAR))
+                count++;
         }
         for(AbstractCard c : AbstractDungeon.player.hand.group){
-            if(c.cardID.equals("Burn"))
-                burnCount++;
+            if(c.tags.contains(SOLAR))
+                count++;
         }
         for(AbstractCard c : AbstractDungeon.player.discardPile.group){
-            if(c.cardID.equals("Burn"))
-                burnCount++;
+            if(c.tags.contains(SOLAR))
+                count++;
         }
-        return burnCount;
+        setCustomVar("ONYOURMARKCOUNT", count);
+        return count;
     }
 
     public void applyPowers() {
         super.applyPowers();
-        this.rawDescription = this.cardStrings.DESCRIPTION + this.cardStrings.EXTENDED_DESCRIPTION[0] +  countBurns() + this.cardStrings.EXTENDED_DESCRIPTION[1];
-        initializeDescription();
+        updateDescription();
     }
 
     public void onMoveToDiscard() {
-        this.rawDescription = this.cardStrings.DESCRIPTION + this.cardStrings.EXTENDED_DESCRIPTION[0] +  countBurns() + this.cardStrings.EXTENDED_DESCRIPTION[1];
-        initializeDescription();
+        updateDescription();
     }
 
     public void calculateCardDamage(AbstractMonster mo) {
         super.calculateCardDamage(mo);
-        this.rawDescription = this.cardStrings.DESCRIPTION + this.cardStrings.EXTENDED_DESCRIPTION[0] +  countBurns() + this.cardStrings.EXTENDED_DESCRIPTION[1];
-        initializeDescription();
+        updateDescription();
     }
 
     @Override
     public void upgrade() {
         super.upgrade();
-        this.rawDescription = this.cardStrings.DESCRIPTION + this.cardStrings.EXTENDED_DESCRIPTION[0] + 0 + this.cardStrings.EXTENDED_DESCRIPTION[1];
+        updateDescription();
+    }
+
+
+    private void updateDescription(){
+        if(!this.upgraded){
+            this.rawDescription = this.cardStrings.DESCRIPTION + this.cardStrings.EXTENDED_DESCRIPTION[0] + countSolar() + this.cardStrings.EXTENDED_DESCRIPTION[1];
+        }else{
+            this.rawDescription = this.cardStrings.UPGRADE_DESCRIPTION + this.cardStrings.EXTENDED_DESCRIPTION[0] + countSolar() + this.cardStrings.EXTENDED_DESCRIPTION[1];
+        }
         this.initializeDescription();
     }
 
