9f7d55afd4f1fc45ecd29bf72d551e971b2e8cb1
GLOWY\Me <lily.outmesguine@gmail.com>
2025-11-25 15:33:57 -0800

    Fixed cardback path in BaseCard

--- src/main/java/TheLightbearer/cards/BaseCard.java
+++ src/main/java/TheLightbearer/cards/BaseCard.java
@@ -93,7 +93,7 @@ public abstract class BaseCard extends CustomCard {
         this.blockUpgrade = 0;
         this.magicUpgrade = 0;
         setBackgroundTexture("TheLightbearer/images/character/cardback/" + element + "/" + type.toString().toLowerCase() + "_" + element + ".png",
-                "TheLightbearer/character/images/cardback/" + element + "/" + type.toString().toLowerCase() + "_" + element +  "_p.png");
+                "TheLightbearer/images/character/cardback/" + element + "/" + type.toString().toLowerCase() + "_" + element +  "_p.png");
         //if(this.tags.contains(SUPERSPELL)){
         //    setBannerTexture("TheLightbearer/character/character/cardback/super/attack_super_banner.png", "TheLightbearer/character/character/cardback/super/attack_super_banner_p.png");
         //}
