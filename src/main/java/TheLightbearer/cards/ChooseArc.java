002ec1048ce8e2932525ae068d2a1908a6f37d39
GLOWY\Me <lily.outmesguine@gmail.com>
2026-01-06 18:12:53 -0800

    Added art for Liquid Fate choices

--- src/main/java/TheLightbearer/cards/ChooseArc.java
+++ src/main/java/TheLightbearer/cards/ChooseArc.java
@@ -23,7 +23,7 @@ public class ChooseArc extends BaseCard {
     public static final String ID = makeID("ChooseArc");
     private static final CardStats info = new CardStats(
             LightbearerCharacter.Enums.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
-            CardType.STATUS, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.NONE, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
             1
